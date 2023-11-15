3db45416cf32bacc92d027b3d6a71fd4a3faffe1
Sanskar Modi <modisanskar5@gmail.com>
2023-11-15 18:45:15 +0530

    Landing Page Added

--- client/src/rbac/constants/doctor.js
+++ client/src/rbac/constants/doctor.js
@@ -1,14 +1,21 @@
 import Home from '../../pages/Home/Home';
+import Landing from '../../pages/Landing/landing';
 
-const DOCTOR_ROUTES = [{
-        link: '/',
-        name: 'User home',
+const DOCTOR_ROUTES = [
+    // {
+    //       link: '/',
+    //       name: 'User home',
+    //       component: < Home / >
+    //   },
+    {
+        link: '/signUp',
+        name: 'SignUp',
         component: < Home / >
     },
     {
         link: '/',
-        name: 'SignUp',
-        component: < Home / >
+        name: 'Landing',
+        component: < Landing / >
     }
 ];
 
