0d9ca4853e1e1b105f9319ba1f5e10e117318d5e
Sanskar Modi <modisanskar5@gmail.com>
2023-11-15 17:53:47 +0530

    Some Assets added

--- client/src/rbac/constants/doctor.js
+++ client/src/rbac/constants/doctor.js
@@ -1,11 +1,15 @@
 import Home from '../../pages/Home/Home';
 
-const DOCTOR_ROUTES = [
-  {
-    link: '/',
-    name: 'User home',
-    component: <Home />
-  }
+const DOCTOR_ROUTES = [{
+        link: '/',
+        name: 'User home',
+        component: < Home / >
+    },
+    {
+        link: '/',
+        name: 'SignUp',
+        component: < Home / >
+    }
 ];
 
 export default DOCTOR_ROUTES;
