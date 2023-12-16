9c393b4417902a952514a19bfafff98d78376784
Bharat Kumar <kumarbharat661@gmail.com>
2023-12-17 02:36:36 +0530

    contract all set

--- ethereum/contracts/HealthChain.sol
+++ ethereum/contracts/HealthChain.sol
@@ -3,16 +3,18 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract HealthChain {
-    string role_docter="Doctor";
-    string role_patient="Patient";
+    enum Role {
+        Doctor, Patient
+    }
+    mapping(address => Role) public roles;
     struct Record {
         string name;
         string url;
         string upload_date;
     }
     
-    struct Illness {
-        string ill_id;
+    struct Disease {
+        string id;
         string name;
         address doctor;
         uint256 created_at;
@@ -34,25 +36,25 @@ contract HealthChain {
         address[] patients;
     }
     
-    struct IllnessRes {
-        string ill_id;
+    struct DiseaseRes {
+        string id;
         string name;
         Doctor doctor;
         uint256 created_at;
     }
     mapping(address => Patient) public patients;//all patients
     mapping(address => Doctor) public doctors;//all doctors
-    mapping(string => Illness) public illnuses;//all illnuses
-    mapping(address => Illness[]) public patients_illness_map;//patients illnuses
-    mapping(string => Record[]) public illness_record_map; //illness to records map
-    mapping(address => string) public person_roles;
+    mapping(string => Disease) public diseases;//all diseases
+    mapping(address => Disease[]) public patients_disease_map;//patients diseases
+    mapping(string => Record[]) public diesase_record_map; //illness to records map
 
     //get initial status
     function getStatus() public view returns (string memory) {
-        string storage role = person_roles[msg.sender];
-        if (keccak256(abi.encodePacked(role)) == keccak256(abi.encodePacked(role_docter)) || keccak256(abi.encodePacked(role)) == keccak256(abi.encodePacked(role_patient))) {
-            return role;
-        } else {
+        if(roles[msg.sender] == Role.Doctor){
+            return "Doctor";
+        }else if(roles[msg.sender] == Role.Patient){
+            return "Patient";
+        }else{
             return "Unregistered";
         }
     }
@@ -61,15 +63,15 @@ contract HealthChain {
     function getDoctor() public view returns (Doctor memory) {
         return doctors[msg.sender];
     }
-    function createDoctor(string memory name , uint256 age , string memory gender, string memory hospital_name) public {
+    function addDoctor(string memory name , uint256 age , string memory gender, string memory hospital_name) public {
         Doctor memory newDoc;
         newDoc.name = name;
         newDoc.wallet_address = msg.sender;
         newDoc.age = age;
         newDoc.gender = gender;
         newDoc.hospital_name = hospital_name;
-        doctors[msg.sender] = newDoc;
-        person_roles[msg.sender] = role_docter;
+        doctors[msg.sender] = newDoc; 
+        roles[msg.sender] = Role.Doctor;
     }
     function getAllPatients() public view returns (Patient[] memory) {
         Doctor memory curr_doctor = doctors[msg.sender];
@@ -80,74 +82,58 @@ contract HealthChain {
         return doctorPatients;
     }
     
-    function getPatientsIllness(address patient_address) public view returns (Illness[] memory) {
-        return patients_illness_map[patient_address];
+    function getPatientsDiseases(address patient_address) public view returns (Disease[] memory) {
+        return patients_disease_map[patient_address];
     }
 
     // Patient functions
-    function createPatient(string memory name, uint256 age, string memory gender) public {
+    function addPatient(string memory name, uint256 age, string memory gender) public {
         Patient memory newPatient;
         newPatient.name = name;
         newPatient.age = age;
         newPatient.wallet_address = msg.sender;
         newPatient.gender = gender;
         patients[msg.sender] = newPatient;
-        person_roles[msg.sender] = role_patient;
+        roles[msg.sender] = Role.Patient;
     }
 
     function getPatient() public view returns (Patient memory) {
         return patients[msg.sender];
     }
     
-    function getIllnesses() public view returns (IllnessRes[] memory) {
-        Illness[] memory curr_ills = patients_illness_map[msg.sender];
-        IllnessRes[] memory ans = new IllnessRes[](curr_ills.length);
-        for(uint256 i=0;i<curr_ills.length;i++){
-            ans[i].ill_id = curr_ills[i].ill_id;
-            ans[i].name = curr_ills[i].name;
-            ans[i].created_at = curr_ills[i].created_at;
-            ans[i].doctor = doctors[curr_ills[i].doctor];
+    function getDiseases() public view returns (DiseaseRes[] memory) {
+        Disease[] memory currDiseases = patients_disease_map[msg.sender];
+        DiseaseRes[] memory ans = new DiseaseRes[](currDiseases.length);
+        for(uint256 i=0;i<currDiseases.length;i++){
+            ans[i].id = currDiseases[i].id;
+            ans[i].name = currDiseases[i].name;
+            ans[i].created_at = currDiseases[i].created_at;
+            ans[i].doctor = doctors[currDiseases[i].doctor];
         }
         return ans;
     }
     
     // Add illness for a patient
-    function addIllness(string memory ill_id, string memory illnessName, address doctor_address) public {
-        Illness memory newIllness ;
-        newIllness.ill_id = ill_id;
-        newIllness.name = illnessName;
-        newIllness.doctor = doctor_address;
+    function addDisease(string memory id, string memory diseaseName, address doctor_address) public {
+        Disease memory newDisease ;
+        newDisease.id = id;
+        newDisease.name = diseaseName;
+        newDisease.doctor = doctor_address;
         //doctor ke pass array hai patient ka map me update that
-        Doctor memory doctor_to_add = doctors[doctor_address];
-        address[] memory old_patients = doctor_to_add.patients;
-        address[] memory new_patients = new address[](old_patients.length+1);
-        for(uint256 i =0;i<old_patients.length;i++){
-            new_patients[i]  = old_patients[i];
-        }
-        new_patients[old_patients.length] = msg.sender;
+        doctors[doctor_address].patients.push(msg.sender);
         //end
-        newIllness.created_at = block.timestamp;
-        illnuses[ill_id] = newIllness;
-        Illness[] memory curr_illnuses = patients_illness_map[msg.sender];
-        Illness[] memory new_ills = new Illness[](curr_illnuses.length+1);
-        for(uint256 i =0;i<curr_illnuses.length;i++){
-            new_ills[i] = curr_illnuses[i];
-        }
-        new_ills[curr_illnuses.length] = newIllness;
+        newDisease.created_at = block.timestamp;
+        diseases[id] = newDisease;
+        patients_disease_map[msg.sender].push(newDisease);
     }
-    function addRecord(string memory name , string memory url , string memory upload_date , string memory ill_id)public view{
+    function addRecord(string memory name , string memory url , string memory upload_date , string memory id)public {
         Record memory newRec;
         newRec.name = name;
         newRec.url = url;
         newRec.upload_date = upload_date;
-        Record[] memory curr_reports = illness_record_map[ill_id];
-        Record[] memory new_reports = new Record[](curr_reports.length+1);
-        for(uint256 i =0;i<curr_reports.length;i++){
-            new_reports[i] = curr_reports[i];
-        }
-        new_reports[curr_reports.length] = newRec;
+        diesase_record_map[id].push(newRec);
     }    
-    function getRecords(string memory ill_id)public view returns(Record[] memory){
-        return illness_record_map[ill_id];
+    function getRecords(string memory id)public view returns(Record[] memory){
+        return diesase_record_map[id];
     }
 }
---
+++ ethereum/test/test.js
@@ -0,0 +1,69 @@
+// test/HealthChain.test.js
+
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("-> HealthChain Test", function () {
+  let healthChain, doctor, patient, owner;
+
+  beforeEach(async () => {
+    healthChain = await ethers.deployContract("HealthChain");
+
+    const signers = await ethers.getSigners();
+    doctor = signers[0];
+    patient = signers[1];
+    owner = signers[2];
+  });
+
+  it("should register a doctor", async () => {
+    await healthChain
+      .connect(doctor)
+      .addDoctor("Dr. Smith", 40, "male", "City Hospital");
+    const doc = await healthChain.connect(doctor).getDoctor();
+    const role = await healthChain.connect(doctor).getStatus();
+    expect(role).to.equal("Doctor");
+    expect(doc.name).to.equal("Dr. Smith");
+    expect(doc.wallet_address).to.equal(doctor.address);
+  });
+
+  it("should register a patient", async () => {
+    await healthChain.connect(patient).addPatient("Alice", 25, "female");
+    const pat = await healthChain.connect(patient).getPatient();
+    const role = await healthChain.connect(patient).getStatus();
+    expect(role).to.equal("Patient");
+    expect(pat.name).to.equal("Alice");
+    expect(pat.wallet_address).to.equal(patient.address);
+  });
+
+  it("should allow patient to add disease and doctors to check there patients ", async () => {
+    const diseaseId = "ajdshkahskdh";
+    await healthChain
+      .connect(doctor)
+      .addDoctor("Dr. Smith", 40, "male", "City Hospital");
+    await healthChain.connect(patient).addPatient("Alice", 25, "female");
+    await healthChain
+      .connect(patient)
+      .addDisease(diseaseId, "Cancer", doctor.address);
+    const patients = await healthChain.connect(doctor).getAllPatients();
+    expect(patients[0].wallet_address).to.equal(patient.address);
+
+    const patientDiseases = await healthChain
+      .connect(doctor)
+      .getPatientsDiseases(patient.address);
+    expect(patientDiseases[0].id).to.equal(diseaseId);
+
+    const diseaseList = await healthChain.connect(patient).getDiseases();
+    expect(diseaseList.map((obj) => obj.id)).to.have.members(
+      patientDiseases.map((obj) => obj.id)
+    );
+
+    //testing addition of records
+    const doc_hash = "hajdslaksd//:";
+    await healthChain
+      .connect(patient)
+      .addRecord("Blood Test", doc_hash, "17/12/2023", diseaseId);
+    const records = await healthChain.connect(patient).getRecords(diseaseId);
+    expect(doc_hash).to.equal(records[0].url);
+    expect("Blood Test").to.equal(records[0].name);
+  });
+});
